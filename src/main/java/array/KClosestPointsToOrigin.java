812031386154e86d4a001d81c0e60dcb5c7bb097
louisrj <rahul.louis2@gmail.com>
2020-01-25 02:38:44 -0800

    Updating

---
+++ src/main/java/array/KClosestPointsToOrigin.java
@@ -0,0 +1,38 @@
+package array;
+
+import strings.IntegerToEnglishWords;
+
+import java.util.*;
+
+public class KClosestPointsToOrigin {
+  public static void main(String[] args) {
+    KClosestPointsToOrigin k = new KClosestPointsToOrigin();
+    int[][] a = new int[][]{
+        {1, 3},
+        {-2, 2}
+    };
+    int[][] ints = k.kClosest(a, 1);
+    for (int[] anInt : ints) {
+      for (int j = 0; j < ints[0].length; j++) {
+        System.out.print(anInt[j]);
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
+  }
+
+  private int[][] kClosest(int[][] points, int K) {
+    PriorityQueue<int[]> pq = new PriorityQueue<>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+    for (int[] p : points) {
+      pq.offer(p);
+      if (pq.size() > K) {
+        pq.poll();
+      }
+    }
+    int[][] res = new int[K][2];
+    while (K > 0) {
+      res[--K] = pq.poll();
+    }
+    return res;
+  }
+}
--- src/main/java/strings/IntegerToEnglishWords.java
+++ src/main/java/strings/IntegerToEnglishWords.java
@@ -1,140 +1,39 @@
 package strings;
 
 public class IntegerToEnglishWords {
+  private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+  private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+  private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+
   public static void main(String[] args) {
     IntegerToEnglishWords integerToEnglishWords = new IntegerToEnglishWords();
     System.out.println(integerToEnglishWords.numberToWords(1234567890));
   }
 
-  private String one(int num) {
-    switch (num) {
-      case 1:
-        return "One";
-      case 2:
-        return "Two";
-      case 3:
-        return "Three";
-      case 4:
-        return "Four";
-      case 5:
-        return "Five";
-      case 6:
-        return "Six";
-      case 7:
-        return "Seven";
-      case 8:
-        return "Eight";
-      case 9:
-        return "Nine";
+  private String numberToWords(int num) {
+    if (num == 0) return "Zero";
+
+    int i = 0;
+    String words = "";
+
+    while (num > 0) {
+      if (num % 1000 != 0)
+        words = helper(num % 1000) + THOUSANDS[i] + " " + words;
+      num /= 1000;
+      i++;
     }
-    return "";
+
+    return words.trim();
   }
 
-  private String twoLessThan20(int num) {
-    switch (num) {
-      case 10:
-        return "Ten";
-      case 11:
-        return "Eleven";
-      case 12:
-        return "Twelve";
-      case 13:
-        return "Thirteen";
-      case 14:
-        return "Fourteen";
-      case 15:
-        return "Fifteen";
-      case 16:
-        return "Sixteen";
-      case 17:
-        return "Seventeen";
-      case 18:
-        return "Eighteen";
-      case 19:
-        return "Nineteen";
-    }
-    return "";
-  }
-
-  private String ten(int num) {
-    switch (num) {
-      case 2:
-        return "Twenty";
-      case 3:
-        return "Thirty";
-      case 4:
-        return "Forty";
-      case 5:
-        return "Fifty";
-      case 6:
-        return "Sixty";
-      case 7:
-        return "Seventy";
-      case 8:
-        return "Eighty";
-      case 9:
-        return "Ninety";
-    }
-    return "";
-  }
-
-  private String two(int num) {
+  private String helper(int num) {
     if (num == 0)
       return "";
-    else if (num < 10)
-      return one(num);
     else if (num < 20)
-      return twoLessThan20(num);
-    else {
-      int tenner = num / 10;
-      int rest = num - tenner * 10;
-      if (rest != 0)
-        return ten(tenner) + " " + one(rest);
-      else
-        return ten(tenner);
-    }
-  }
-
-  private String three(int num) {
-    int hundred = num / 100;
-    int rest = num - hundred * 100;
-    String res = "";
-    if (hundred * rest != 0)
-      res = one(hundred) + " Hundred " + two(rest);
-    else if ((hundred == 0) && (rest != 0))
-      res = two(rest);
-    else if ((hundred != 0) && (rest == 0))
-      res = one(hundred) + " Hundred";
-    return res;
-  }
-
-  private String numberToWords(int num) {
-    if (num == 0)
-      return "Zero";
-
-    int billion = num / 1000000000;
-    int million = (num - billion * 1000000000) / 1000000;
-    int thousand = (num - billion * 1000000000 - million * 1000000) / 1000;
-    int rest = num - billion * 1000000000 - million * 1000000 - thousand * 1000;
-
-    String result = "";
-    if (billion != 0)
-      result = three(billion) + " Billion";
-    if (million != 0) {
-      if (!result.isEmpty())
-        result += " ";
-      result += three(million) + " Million";
-    }
-    if (thousand != 0) {
-      if (!result.isEmpty())
-        result += " ";
-      result += three(thousand) + " Thousand";
-    }
-    if (rest != 0) {
-      if (!result.isEmpty())
-        result += " ";
-      result += three(rest);
-    }
-    return result;
+      return LESS_THAN_20[num] + " ";
+    else if (num < 100)
+      return TENS[num / 10] + " " + helper(num % 10);
+    else
+      return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
   }
 }
